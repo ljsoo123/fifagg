39113bf26e84482a36e0632aa5cbef1b0dce8a6a
ljsoo123 <ljsoo123456@naver.com>
2020-10-15 23:46:33 +0900

    [CREATE] 플레이어 정보 style

---
+++ src/Player/styles.js
@@ -0,0 +1,21 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const Nickname = styled.div`
+  font-size: 50px;
+`;
+
+export const Level = styled.div`
+  font-size: 30px;
+`;
+
+export const Main = styled.div`
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Global = createGlobalStyle`
+    body {
+        margin : 0;
+    }
+`;
