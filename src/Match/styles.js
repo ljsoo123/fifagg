f99af92cd89f00ee26f2b4b5de9713c58132c226
ljsoo123 <ljsoo123456@naver.com>
2020-10-15 23:46:56 +0900

    [CREATE] 경기 결과 style

---
+++ src/Match/styles.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  > div {
+    margin: 10px;
+    > div {
+      margin: 10px;
+    }
+  }
+`;
+
+export const FirstPlayer = styled.div`
+  display: flex;
+  > div {
+    margin: 10px;
+  }
+`;
+
+export const SecondPlayer = styled.div`
+  display: flex;
+  > div {
+    margin: 10px;
+  }
+`;
+
+export const Vs = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const List = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
