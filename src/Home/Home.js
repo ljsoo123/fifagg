d5ebae705838d5ecad33e8502e82f250e3086ad1
ljsoo123 <ljsoo123456@naver.com>
2020-10-15 23:47:22 +0900

    [CREATE] HOME

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import * as S from "./styles";
 
-const Home = () => {
-    return()
-}
+const Home = ({ history }) => {
+  const inputRef = useRef(null);
+  const onBtnClick = () => {
+    history.push(`/Profile/${inputRef.current.value}`);
+  };
+  return (
+    <S.Main>
+      <input ref={inputRef} />
+      <button onClick={onBtnClick} />
+    </S.Main>
+  );
+};
+
+export default Home;
