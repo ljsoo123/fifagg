820b4d4a41b7b6df302aef6adf0d96f60a27c925
ljsoo123 <ljsoo123456@naver.com>
2020-10-26 11:34:42 +0900

    [CREATE] 상세정보 컴포넌트 생성

---
+++ src/Information/Information.js
@@ -0,0 +1,11 @@
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import * as S from "./styles";
+
+const Information = ({ match, matchIn }) => {
+  console.log(match);
+  console.log(match.params);
+  return <S.MainDiv>{}</S.MainDiv>;
+};
+
+export default Information;
