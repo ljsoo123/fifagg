83f482bb8d7aab659b82c318442867111068554f
ljsoo123 <ljsoo123456@naver.com>
2020-11-02 22:45:08 +0900

    [MODIFY] style change

--- src/Match/styles.js
+++ src/Match/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const MainDiv = styled.div`
+  z-index: 0;
   > div {
     margin: 10px;
     > div {
@@ -35,8 +36,27 @@ export const List = styled.div`
   align-items: center;
 `;
 
+export const InfDiv = styled.div`
+  position: fixed;
+  top: 0;
+  display: flex;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.6);
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
+
 export const Inf = styled.div`
-  width: 0;
-  height: 0;
-  display: none;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  width: 80%;
+  margin: 0 auto;
+  z-index: 100000;
+  position: relative;
+  box-sizing: border-box;
+  background: white;
 `;
